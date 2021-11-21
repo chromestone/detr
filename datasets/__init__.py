8672a2813bbd164ba7fcc722129431c8f0ce5ae7
Derek Zhang <derekzhang@Dereks-MacBook-Pro.local>
2021-11-21 18:45:16 -0500

    pascal voc dataset support
    I hope this works

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,7 @@ import torch.utils.data
 import torchvision
 
 from .coco import build as build_coco
+from .pascalvoc import build as build_voc
 
 
 def get_coco_api_from_dataset(dataset):
@@ -22,4 +23,6 @@ def build_dataset(image_set, args):
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
         return build_coco_panoptic(image_set, args)
+    if args.dataset_file == 'pascalvoc':
+        return build_voc(image_set, args)
     raise ValueError(f'dataset {args.dataset_file} not supported')
